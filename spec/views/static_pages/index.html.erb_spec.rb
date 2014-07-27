2f1256c454f199f164d06c9ef5023239228a21dc
A.J. Norton <ajn123@vt.edu>
2014-07-27 14:09:42 -0700

    all tests pass

--- spec/views/static_pages/index.html.erb_spec.rb
+++ spec/views/static_pages/index.html.erb_spec.rb
@@ -10,30 +10,30 @@ RSpec.describe "static_pages/index.html.erb", :type => :view do
 
     it "should have the correct links" do
       click_link "About"
-      expect(page).to have_content('about')
+      expect(page).to have_content('About')
       click_link "Contact"
-      expect(page).to have_content('contact')
+      expect(page).to have_content('Coming soon')
       click_link "Updates"
-      expect(page).to have_content('updates')
+      expect(page).to have_content('Updates')
     end
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('about')}
+    it { should have_content('About')}
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('contact') }
+    it { should have_content('Coming soon') }
   end
 
   describe "Update page" do
     before { visit updates_path }
 
-    it { should have_content('updates') }
+    it { should have_content('Updates') }
   end
 
 end
