25d9b6053dcca3eba669038f42aeb4345988ecac
A.J. Norton <ajn123@vt.edu>
2014-07-31 01:59:40 -0700

    corrected updates spelling

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe NotesController, :type => :controller do
 
-  describe "GET show" do
-    it "returns http success" do
-      get :show
-      expect(response).to be_success
-    end
-  end
 
   describe "GET new" do
     it "returns http success" do
@@ -19,13 +13,6 @@ RSpec.describe NotesController, :type => :controller do
   describe "GET index" do
     it "returns http success" do
       get :index
-      expect(response).to be_success
-    end
-  end
-
-  describe "GET edit" do
-    it "returns http success" do
-      get :edit
       expect(response).to be_success
     end
   end
