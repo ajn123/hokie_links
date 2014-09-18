83ba4bf713da781bf8cf70045b0a9c383364dc6e
alexansc <aekschwartzberg@gmail.com>
2014-09-18 19:30:01 -0400

    Typeahead JS and CSS fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,11 @@
 // about supported directives.
 //
 
-//= require jquery-2.1.1
-//= require twitter/typeahead
+//= require jquery
+//= require jquery_ujs
 //= require handlebars.runtime
 //= require handlebars
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require twitter/typeahead
 //= require_tree .
